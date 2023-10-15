c724f5d31020dcfacdbb84a132f4fbcc0476572c
Tazhiyeva <36223969+Tazhiyeva@users.noreply.github.com>
2023-10-15 21:14:25 +0600

    Update goreloaded_test.go

--- goreloaded_test.go
+++ goreloaded_test.go
@@ -1,61 +1,49 @@
 package main
-
 import (
 	"testing"
 )
-
 type startedWithVowelTest struct {
 	nextWordAfterArticle string
 	expected             bool
 }
-
 type extractNumberFromCommandTest struct {
 	command  string
 	expected int
 }
-
 type convertHexToDecimal struct {
 	hexNumber string
 	expected  string
 }
-
 type convertBinToDecimal struct {
 	binNumber string
 	expected  string
 }
-
 type replaceApostrophe struct {
 	originalText string
 	expected     string
 }
-
 type handleSingleQuotes struct {
 	originalText string
 	expected     string
 }
-
 type fixPunctuation struct {
 	originalText string
 	expected     string
 }
-
 type replaceArticles struct {
 	originalText string
 	expected     string
 }
-
 type getFormattedText struct {
 	originalText string
 	expected     string
 }
-
 var startedWithVowelTests = []startedWithVowelTest{
 	{nextWordAfterArticle: "eight", expected: true},
 	{nextWordAfterArticle: "hour", expected: true},
 	{nextWordAfterArticle: "five", expected: false},
 	{nextWordAfterArticle: "1eye", expected: false},
 }
-
 var extractNumberFromCommandTests = []extractNumberFromCommandTest{
 	{command: "(cap)", expected: 1},
 	{command: "(cap, 1)", expected: 1},
@@ -65,19 +53,16 @@ var extractNumberFromCommandTests = []extractNumberFromCommandTest{
 	{command: "(cap, hh)", expected: 1},
 	{command: "(cap,)", expected: 1},
 }
-
 var convertHexToDecimalTests = []convertHexToDecimal{
 	{hexNumber: "1E", expected: "30"},
 	{hexNumber: "EEEE", expected: "61166"},
 	{hexNumber: "The", expected: "The"},
 }
-
 var convertBinToDecimalTests = []convertBinToDecimal{
 	{binNumber: "1E", expected: "1E"},
 	{binNumber: "101", expected: "5"},
 	{binNumber: "0000", expected: "0"},
 }
-
 var replaceApostropheTests = []replaceApostrophe{
 	{originalText: "Hello! It's me", expected: "Hello! It‘s me"},
 	{originalText: "Somebody's dog", expected: "Somebody‘s dog"},
@@ -86,7 +71,6 @@ var replaceApostropheTests = []replaceApostrophe{
 	{originalText: "I'll see", expected: "I‘ll see"},
 	{originalText: "we‘ll see", expected: "we‘ll see"},
 }
-
 var handleSingleQuotesTests = []handleSingleQuotes{
 	{originalText: "actually ' hi'", expected: "actually 'hi'"},
 	{originalText: "'hi ' ' hi'", expected: "'hi' 'hi'"},
@@ -94,15 +78,13 @@ var handleSingleQuotesTests = []handleSingleQuotes{
 	{originalText: "it‘s ' smart '", expected: "it‘s 'smart'"},
 	{originalText: "hello ' hello hello ' hello", expected: "hello 'hello hello' hello"},
 }
-
 var fixPunctuationTests = []fixPunctuation{
 	{originalText: "actually , hi", expected: "actually, hi"},
 	{originalText: "'hi !? hi'", expected: "'hi!? hi'"},
 	{originalText: " , hi she said", expected: ", hi she said"},
 	{originalText: "it‘s. smart : ", expected: "it‘s. smart: "},
-	{originalText: "hello ? hello hello ! hello.", expected: "hello? hello hello! hello."},
+	{originalText: "hello ? hello hello ! hello.", expected: "hello? hello hello! hello. "},
 }
-
 var replaceArticlesTests = []replaceArticles{
 	{originalText: "an apple", expected: "an apple"},
 	{originalText: "a book", expected: "a book"},
@@ -113,7 +95,6 @@ var replaceArticlesTests = []replaceArticles{
 	{originalText: "A book", expected: "A book"},
 	{originalText: "An book", expected: "A book"},
 }
-
 var getFormattedTextTests = []getFormattedText{
 	{
 		originalText: "The (hex) AB (hex) CDEF (hex) 1234 (hex) GHIJKLMN",
@@ -133,7 +114,7 @@ var getFormattedTextTests = []getFormattedText{
 	},
 	{
 		originalText: "(hex) .",
-		expected:     ".",
+		expected:     ". ",
 	},
 	{
 		originalText: "The (bin) 10 (bin) 1010 (bin) 11110 (bin) GHIJKLMN",
@@ -153,7 +134,7 @@ var getFormattedTextTests = []getFormattedText{
 	},
 	{
 		originalText: "(bin) .",
-		expected:     ".",
+		expected:     ". ",
 	},
 	{
 		originalText: "HELLO. (low) I am good",
@@ -185,7 +166,7 @@ var getFormattedTextTests = []getFormattedText{
 	},
 	{
 		originalText: "HELLO (low, 2.) I am good",
-		expected:     "HELLO (low, 2.) I am good",
+		expected:     "HELLO (low, 2. ) I am good",
 	},
 	{
 		originalText: "123 (low) HELLO",
@@ -217,7 +198,7 @@ var getFormattedTextTests = []getFormattedText{
 	},
 	{
 		originalText: "HELLO (up, 2.) I am good",
-		expected:     "HELLO (up, 2.) I am good",
+		expected:     "HELLO (up, 2. ) I am good",
 	},
 	{
 		originalText: "123 (up) HELLO",
@@ -229,31 +210,31 @@ var getFormattedTextTests = []getFormattedText{
 	},
 	{
 		originalText: "HELLO MY BABY, I Missed SO mUch (cap, 5).",
-		expected:     "HELLO MY BABY, I Missed SO MUch.",
+		expected:     "HELLO MY BABY, I Missed SO MUch. ",
 	},
 	{
 		originalText: "I LIKE to (cap) read (cap) BOOKS.",
-		expected:     "I LIKE To Read BOOKS.",
+		expected:     "I LIKE To Read BOOKS. ",
 	},
 	{
 		originalText: "Hello , i like to (cap) read. (cap,2) BOOKS.",
-		expected:     "Hello, i like To Read. BOOKS.",
+		expected:     "Hello, i like To Read. BOOKS. ",
 	},
 	{
 		originalText: "hello (cap, 10).",
-		expected:     "Hello.",
+		expected:     "Hello. ",
 	},
 	{
 		originalText: "HELLO (cap, ABC).",
-		expected:     "HELLO (cap, ABC).",
+		expected:     "HELLO (cap, ABC). ",
 	},
 	{
 		originalText: "HELLO (cap, 2.) I am good.",
-		expected:     "HELLO (cap, 2.) I am good.",
+		expected:     "HELLO (cap, 2. ) I am good. ",
 	},
 	{
 		originalText: "123 (cap) HELLO.",
-		expected:     "123 HELLO.",
+		expected:     "123 HELLO. ",
 	},
 	{
 		originalText: "one (up) , two , three (cap,2), ' four' , an FIVE, (low) ' six ,seven ?! a eight' 00111 (bin) FFF (hex) The end ... ",
@@ -261,10 +242,9 @@ var getFormattedTextTests = []getFormattedText{
 	},
 	{
 		originalText: "I was sitting over there ,and then BAMM !!",
-		expected:     "I was sitting over there, and then BAMM!!",
+		expected:     "I was sitting over there, and then BAMM!! ",
 	},
 }
-
 func TestIsStartedWithVowel(t *testing.T) {
 	for _, test := range startedWithVowelTests {
 		if output := IsStartedWithVowel(test.nextWordAfterArticle); output != test.expected {
@@ -272,7 +252,6 @@ func TestIsStartedWithVowel(t *testing.T) {
 		}
 	}
 }
-
 func TestExtractNumberFromCommand(t *testing.T) {
 	for _, test := range extractNumberFromCommandTests {
 		if output := ExtractNumberFromCommand(test.command); output != test.expected {
@@ -280,52 +259,41 @@ func TestExtractNumberFromCommand(t *testing.T) {
 		}
 	}
 }
-
 func TestToUpper(t *testing.T) {
 	actual := ToUpper("hello")
 	expected := "HELLO"
-
 	if actual != expected {
 		t.Errorf("got %q, wanted %q", actual, expected)
 	}
 }
-
 func TestToLower(t *testing.T) {
 	actual := ToLower("HeLLo")
 	expected := "hello"
-
 	if actual != expected {
 		t.Errorf("got %q, wanted %q", actual, expected)
 	}
 }
-
 func TestToCapitalize(t *testing.T) {
 	actual := ToCapitalize("heLLo")
 	expected := "HeLLo"
-
 	if actual != expected {
 		t.Errorf("got %q, wanted %q", actual, expected)
 	}
 }
-
 func TestConvertHexToDecimal(t *testing.T) {
 	for _, test := range convertHexToDecimalTests {
 		if output := ConvertHexToDecimal(test.hexNumber); output != test.expected {
 			t.Errorf("actual: %v, expected: %v", output, test.expected)
 		}
 	}
-
 }
-
 func TestConvertBinToDecimal(t *testing.T) {
 	for _, test := range convertBinToDecimalTests {
 		if output := ConvertBinToDecimal(test.binNumber); output != test.expected {
 			t.Errorf("actual: %v, expected: %v", output, test.expected)
 		}
 	}
-
 }
-
 func TestReplaceApostrophe(t *testing.T) {
 	for _, test := range replaceApostropheTests {
 		if output := ReplaceApostrophe(test.originalText); output != test.expected {
@@ -333,7 +301,6 @@ func TestReplaceApostrophe(t *testing.T) {
 		}
 	}
 }
-
 func TestHandleSingleQuotes(t *testing.T) {
 	for _, test := range handleSingleQuotesTests {
 		if output := HandleSingleQuotes(test.originalText); output != test.expected {
@@ -341,7 +308,6 @@ func TestHandleSingleQuotes(t *testing.T) {
 		}
 	}
 }
-
 func TestFixPunctuation(t *testing.T) {
 	for _, test := range fixPunctuationTests {
 		if output := FixPunctuation(test.originalText); output != test.expected {
@@ -349,7 +315,6 @@ func TestFixPunctuation(t *testing.T) {
 		}
 	}
 }
-
 func TestReplaceArticles(t *testing.T) {
 	for _, test := range replaceArticlesTests {
 		if output := ReplaceArticles(test.originalText); output != test.expected {
@@ -357,7 +322,6 @@ func TestReplaceArticles(t *testing.T) {
 		}
 	}
 }
-
 func TestGetFormattedText(t *testing.T) {
 	for _, test := range getFormattedTextTests {
 		if output := GetFormattedText(test.originalText); output != test.expected {
